6fb5bc760f56dcf5dbe2ab7644fc4890502af753
theScrlLkKey <microman5252@gmail.com>
2023-06-16 01:50:19 -0700

    fixed emoji problem with refactor

--- chat-player.py
+++ chat-player.py
@@ -4,16 +4,18 @@ import json
 import requests
 from datetime import timedelta
 from xml.etree import ElementTree
+from collections import deque
 
 # TODO:
 # grab current elapsed time from VLC web XML doc, then feed that in. - done
 # get title, if title changes change file we are reading from
-# display donations
-# make emojis lag less
 # maybe attempt to compensate for lag, offset is included in live chat json
+# display donations liveChatPaidMessageRenderer and paid_message (check for existance of liveChatTextMessageRenderer, then check for other types)
 
 chat_data_clean = []
-chat_data = []
+chat_data = deque()
+
+last_time = 0
 
 # load vlc password from password.txt
 with open('password.txt') as file_pw:
@@ -45,7 +47,7 @@ def load_chat(video_id):
                         ).total_seconds()
                     else:  # display pre-chat
                         total_seconds = -1
-                    chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["timestampText"]["parsed"] = total_seconds
+                    chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["timestampText"]["parsed"] = int(total_seconds)
                     chat_data.append(chat_message)
                 except KeyError:
                     # Handle missing keys or other errors
@@ -55,12 +57,12 @@ def load_chat(video_id):
         print("Video has no chat to replay")
 
 
-# Callback function to display chat messages
+# function to display chat messages
 def display_content(seconds):
     global last_time
     global chat_data
     current_time = int(seconds)  # seconds
-    indexes_remove = []
+    messages_to_print = []
 
     # on rewind, clear screen and refill chat messages
     if current_time < last_time:
@@ -68,55 +70,29 @@ def display_content(seconds):
         os.system('cls' if os.name == 'nt' else 'clear')
 
     # Display chat messages
-    for chat_message in chat_data:
-        try:
-            # get timestamp in seconds
-            chat_timestamp = int(chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["timestampText"]["parsed"])
-            # if chat message should be displayed, print it, and the add it into the index of messages we have printed
-            if chat_timestamp <= current_time:
-                # because youtube is actually stupid, messages with emojis are split into
-                # ["message"]["runs"][0]["text"] ["message"]["runs"][1]["emoji"]["emojiId"] etc
-                message_parsed = ""
-                # not super great code here
-                for i in range(len(chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["message"]["runs"])):
-                    try:
-                        message_parsed += chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["message"]["runs"][i]["text"]
-                    except KeyError:
-                        try:
-                            message_parsed += chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["message"]["runs"][i]["emoji"]["emojiId"]
-                        except KeyError:
-                            input("something other than emoji or text?")
-                            print(message_parsed)
-                            exit()
-                print("[", chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["timestampText"]["simpleText"], chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["authorName"]["simpleText"], "]", message_parsed)
-                indexes_remove.append(int(chat_data.index(chat_message)))
-            else:
-                # now we have printed all the current messages, so we remove them
-                for i in sorted(indexes_remove, reverse=True):
-                    del chat_data[i]
-                break
-        except KeyError as err:
-            # Handle missing keys or other errors
-            # print("Skipping chat message due to missing keys:", chat_message)
-            # bots/donos? unsure
-            # attempt to print paid message
-            try:
-                # get timestamp in seconds
-                input(chat_message)
-                chat_timestamp = int(chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatPaidMessageRenderer"]["timestampText"]["parsed"])
-                print("got here")
-                # if chat message should be displayed, print it, and the add it into the index of messages we have printed
-                if chat_timestamp <= current_time:
-                    print("[", chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatPaidMessageRenderer"]["timestampText"]["simpleText"], chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatPaidMessageRenderer"]["purchaseAmountText"]["simpleText"], chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatPaidMessageRenderer"]["authorName"]["simpleText"], "]", chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatPaidMessageRenderer"]["message"]["runs"][0]["text"])
-                    indexes_remove.append(int(chat_data.index(chat_message)))
-            except KeyError as err:
-                # welp, guess it wasn't a dono
-                pass
+    for index, chat_message in enumerate(chat_data):
+        standard_message = chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]
+        timestamp_text = standard_message["timestampText"]["simpleText"]
+        author_name = standard_message["authorName"]["simpleText"]
+        # this is a really impressive performance improvement from a bunch of nested try/catch, dont do that
+        # there may be other types than text or emoji... unsure
+        message_text = "".join(run.get("text", "") or run["emoji"].get("emojiId", "") for run in standard_message["message"]["runs"])
+        chat_timestamp = standard_message["timestampText"]["parsed"]
 
+        if chat_timestamp <= current_time:
+            messages_to_print.append((timestamp_text, author_name, message_text))
+        else:
+            break
+
+    # Print chat messages, then remove them
+    for timestamp_text, author_name, message_text in messages_to_print:
+        print("[", timestamp_text, author_name, "]", message_text)
+        # this should work fine. it feels slightly wrong though?
+        chat_data.popleft()
+
+    # update previous time
     last_time = current_time
 
-
-last_time = 0
 
 # connect to vlc, parse xml to get currently playing video
 vlc_session = requests.Session()
@@ -134,6 +110,11 @@ track_title = track_title.text.split('/')[-1].replace('watch?v=', '')
 
 # Load the JSON chat data
 load_chat(track_title)
+
+# Convert chat_data and chat_data_clean to deque
+# the docs for deque makes it seem like this is good? probably shouldnt trust chatgpt blindly
+chat_data = deque(chat_data)
+chat_data_clean = deque(chat_data_clean)
 
 # mainloop
 while True:
