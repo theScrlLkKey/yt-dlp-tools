c261aacf6a8e256deff22e67a8262c8c5a3d3fe3
theScrlLkKey <microman5252@gmail.com>
2023-06-16 16:28:28 -0700

    reads donos, removes garbage from chat_data

--- chat-player.py
+++ chat-player.py
@@ -8,9 +8,11 @@ from collections import deque
 
 # TODO:
 # grab current elapsed time from VLC web XML doc, then feed that in. - done
-# get title, if title changes change file we are reading from
+# get title, if title changes change file we are reading from - done. i think
+# display donations liveChatPaidMessageRenderer (check for existance of liveChatTextMessageRenderer, then check for other types) - done
+# what is placeholderitem, and what is paidtickeritem
 # maybe attempt to compensate for lag, offset is included in live chat json
-# display donations liveChatPaidMessageRenderer and paid_message (check for existance of liveChatTextMessageRenderer, then check for other types)
+# fix emoji lag - done
 
 chat_data_clean = []
 chat_data = deque()
@@ -25,12 +27,15 @@ with open('password.txt') as file_pw:
 def load_chat(video_id):
     global chat_data
     global chat_data_clean
+    chat_data = deque()
     try:
         with open(f"test/{video_id}.live_chat.json", encoding="utf8") as file:
             for line in file:
-                chat_message = json.loads(line)
                 try:
-                    timestamp_text = chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["timestampText"]["simpleText"]
+                    chat_message = json.loads(line)
+                    # normal or donation
+                    message_parsing = chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"].get("liveChatTextMessageRenderer") or chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"].get("liveChatPaidMessageRenderer")
+                    timestamp_text = message_parsing["timestampText"]["simpleText"]
                     if "-" not in timestamp_text:
                         timestamp_hour, timestamp_min_sec = timestamp_text.split(':', 1)  # split into hour and minsec
                         if ":" in timestamp_min_sec:
@@ -47,12 +52,20 @@ def load_chat(video_id):
                         ).total_seconds()
                     else:  # display pre-chat
                         total_seconds = -1
-                    chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["timestampText"]["parsed"] = int(total_seconds)
-                    chat_data.append(chat_message)
+                    message_parsing["timestampText"]["parsed"] = int(total_seconds)
+                    #input(message_parsing)
+                    chat_data.append(message_parsing)
+                except TypeError:
+                    # Handle missing keys or other errors
+                    print("Skipping chat message due to missing keys:", chat_message)
                 except KeyError:
                     # Handle missing keys or other errors
                     print("Skipping chat message due to missing keys:", chat_message)
         chat_data_clean = chat_data.copy()
+        # Convert chat_data and chat_data_clean to deque
+        # the docs for deque makes it seem like this is good? probably shouldnt trust chatgpt blindly
+        chat_data = deque(chat_data)
+        chat_data_clean = deque(chat_data_clean)
     except FileNotFoundError:
         print("Video has no chat to replay")
 
@@ -70,23 +83,27 @@ def display_content(seconds):
         os.system('cls' if os.name == 'nt' else 'clear')
 
     # Display chat messages
+    # why does it yell at me if i remove index?
     for index, chat_message in enumerate(chat_data):
-        standard_message = chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]
-        timestamp_text = standard_message["timestampText"]["simpleText"]
-        author_name = standard_message["authorName"]["simpleText"]
+        # if this is a donation, get amount
+        paid_amount = chat_message.get("purchaseAmountText", {}).get("simpleText", "")
+        # it must have these
+        timestamp_text = chat_message["timestampText"]["simpleText"]
+        author_name = chat_message["authorName"]["simpleText"]
         # this is a really impressive performance improvement from a bunch of nested try/catch, dont do that
         # there may be other types than text or emoji... unsure
-        message_text = "".join(run.get("text", "") or run["emoji"].get("emojiId", "") for run in standard_message["message"]["runs"])
-        chat_timestamp = standard_message["timestampText"]["parsed"]
+        # just donations have no message
+        message_text = "".join(run.get("text", "") or run["emoji"].get("emojiId", "") for run in chat_message.get("message", {}).get("runs", {}))
+        chat_timestamp = chat_message["timestampText"]["parsed"]
 
         if chat_timestamp <= current_time:
-            messages_to_print.append((timestamp_text, author_name, message_text))
+            messages_to_print.append((timestamp_text, paid_amount, author_name, message_text))
         else:
             break
 
     # Print chat messages, then remove them
-    for timestamp_text, author_name, message_text in messages_to_print:
-        print("[", timestamp_text, author_name, "]", message_text)
+    for timestamp_text, paid_amount, author_name, message_text in messages_to_print:
+        print(f"{paid_amount} [{timestamp_text} | {author_name}] {message_text}")
         # this should work fine. it feels slightly wrong though?
         chat_data.popleft()
 
@@ -111,8 +128,7 @@ track_title = track_title.text.split('/')[-1].replace('watch?v=', '')
 # Load the JSON chat data
 load_chat(track_title)
 
-# Convert chat_data and chat_data_clean to deque
-# the docs for deque makes it seem like this is good? probably shouldnt trust chatgpt blindly
+# i shouldnt need to do this here, it is handled in load_chat()
 chat_data = deque(chat_data)
 chat_data_clean = deque(chat_data_clean)
 
