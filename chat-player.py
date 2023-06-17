9fdbfa7f2cbc21814139214d5fa13bf3a1bded4e
theScrlLkKey <microman5252@gmail.com>
2023-06-16 22:34:17 -0700

    members work

--- chat-player.py
+++ chat-player.py
@@ -10,7 +10,8 @@ from collections import deque
 # grab current elapsed time from VLC web XML doc, then feed that in. - done
 # get title, if title changes change file we are reading from - done. i think
 # display donations liveChatPaidMessageRenderer (check for existance of liveChatTextMessageRenderer, then check for other types) - done
-# what is placeholderitem, and what is paidtickeritem
+# show new members - liveChatTickerSponsorItemRenderer - done
+# what is placeholderitem
 # maybe attempt to compensate for lag, offset is included in live chat json
 # fix emoji lag - done
 
@@ -34,7 +35,12 @@ def load_chat(video_id):
                 try:
                     chat_message = json.loads(line)
                     # normal or donation
-                    message_parsing = chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"].get("liveChatTextMessageRenderer") or chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"].get("liveChatPaidMessageRenderer")
+                    # turns out liveChatTickerPaidMessage just duplicates some liveChatPaidMessage
+                    message_parsing = chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]
+                    # try:
+                    message_parsing = message_parsing.get("liveChatTextMessageRenderer") or message_parsing.get("liveChatPaidMessageRenderer") or message_parsing.get("liveChatMembershipItemRenderer")
+                    # except KeyError:
+                        # message_parsing = chat_message["replayChatItemAction"]["actions"][0]["addLiveChatTickerItemAction"]["item"]["liveChatTickerPaidMessageItemRenderer"]["showItemEndpoint"]["showLiveChatItemEndpoint"]["renderer"].get("liveChatPaidMessageRenderer")
                     timestamp_text = message_parsing["timestampText"]["simpleText"]
                     if "-" not in timestamp_text:
                         timestamp_hour, timestamp_min_sec = timestamp_text.split(':', 1)  # split into hour and minsec
@@ -53,14 +59,18 @@ def load_chat(video_id):
                     else:  # display pre-chat
                         total_seconds = -1
                     message_parsing["timestampText"]["parsed"] = int(total_seconds)
-                    #input(message_parsing)
+                    # easy member parsing (really stupid)
+                    try:
+                        message_parsing["member"] = str(message_parsing["headerSubtext"]["runs"][0]["text"]) + ":"
+                    except KeyError:
+                        message_parsing["member"] = ""
                     chat_data.append(message_parsing)
                 except TypeError:
                     # Handle missing keys or other errors
                     print("Skipping chat message due to missing keys:", chat_message)
                 except KeyError:
                     # Handle missing keys or other errors
-                    print("Skipping chat message due to missing keys:", chat_message)
+                    print("skipping chat message due to missing keys:", chat_message)
         chat_data_clean = chat_data.copy()
         # Convert chat_data and chat_data_clean to deque
         # the docs for deque makes it seem like this is good? probably shouldnt trust chatgpt blindly
@@ -87,23 +97,24 @@ def display_content(seconds):
     for index, chat_message in enumerate(chat_data):
         # if this is a donation, get amount
         paid_amount = chat_message.get("purchaseAmountText", {}).get("simpleText", "")
+        member = chat_message.get("member", "")
         # it must have these
         timestamp_text = chat_message["timestampText"]["simpleText"]
         author_name = chat_message["authorName"]["simpleText"]
         # this is a really impressive performance improvement from a bunch of nested try/catch, dont do that
         # there may be other types than text or emoji... unsure
-        # just donations have no message
+        # pure donos/members have no message
         message_text = "".join(run.get("text", "") or run["emoji"].get("emojiId", "") for run in chat_message.get("message", {}).get("runs", {}))
         chat_timestamp = chat_message["timestampText"]["parsed"]
 
         if chat_timestamp <= current_time:
-            messages_to_print.append((timestamp_text, paid_amount, author_name, message_text))
+            messages_to_print.append((timestamp_text, member, paid_amount, author_name, message_text))
         else:
             break
 
     # Print chat messages, then remove them
-    for timestamp_text, paid_amount, author_name, message_text in messages_to_print:
-        print(f"{paid_amount} [{timestamp_text} | {author_name}] {message_text}")
+    for timestamp_text, member, paid_amount, author_name, message_text in messages_to_print:
+        print(f"{paid_amount}{member} [{timestamp_text} | {author_name}] {message_text}")
         # this should work fine. it feels slightly wrong though?
         chat_data.popleft()
 
