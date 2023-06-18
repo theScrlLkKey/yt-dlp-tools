7cb93037214da697b0a5caddc30b52153e71be7c
theScrlLkKey <microman5252@gmail.com>
2023-06-18 02:06:29 -0700

    info psudocode

--- info-display.py
+++ info-display.py
@@ -8,6 +8,7 @@ from xml.etree import ElementTree
 # display description, like/dislike, views, title, channel, listed status, date
 # open appropriate file from vlc
 # * maybe display thumbnail as ascii art
+# show video progress and volume with ascii blocks
 
 # set defaults
 
@@ -16,9 +17,21 @@ with open('password.txt') as file_pw:
     vlc_password = file_pw.read()
 
 # function to load infojson from video id, and split it into appropriate bits
+def load_info(video_id):
+    # reset variables
+
+    # open file
+
+
 
 # function to display the bits
 
 # mainloop
 while True:
-    # get track xml from vlc
+    # get status xml from vlc, parse to ID (use chat player code)
+
+    # is it the same ID? sleep or update
+
+    # otherwise, load new video metadata
+
+    time.sleep(0.01)
